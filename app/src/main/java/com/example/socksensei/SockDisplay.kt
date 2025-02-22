e7725b78f5231cd0223b58c9ad7be68c6a727196
Ali Meski <ameski@uoguelph.ca>
2025-02-22 15:41:33 -0500

    added typewriter effect

--- app/src/main/java/com/example/socksensei/SockDisplay.kt
+++ app/src/main/java/com/example/socksensei/SockDisplay.kt
@@ -11,10 +11,5 @@ class SockDisplay : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_sock_display)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
