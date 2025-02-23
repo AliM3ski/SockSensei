d2f7deb9e5a5419504059f3ab564cdfd60cd2d8f
Ali Meski <ameski@uoguelph.ca>
2025-02-22 22:45:21 -0500

    test1

--- app/src/main/java/com/example/socksensei/HappyHailey.kt
+++ app/src/main/java/com/example/socksensei/HappyHailey.kt
@@ -11,10 +11,5 @@ class HappyHailey : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_happy_hailey)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
